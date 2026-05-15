c50d6dddaf52abc2ff1b9f5ea0209275a7be90fe
Moritz <schradermoritz@web.de>
2026-05-14 20:49:32 -0400

    follower counter

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -6,7 +6,19 @@ function calculateArea() {
  width = parseFloat(document.getElementById('width').value);
 
 let area = length * width;
+
+document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
-ocument.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+function calculateTotal() {
+    let grocery1 = parseFloat(document.getElementById('grocery1').value);
+    let grocery2 = parseFloat(document.getElementById('grocery2').value);
+    let grocery3 = parseFloat(document.getElementById('grocery3').value);
+
+    let total = grocery1 + grocery2 + grocery3
+
+document.getElementById('total').innerText = `The total amount is :$ ${total}`;
+}
+
+
 
