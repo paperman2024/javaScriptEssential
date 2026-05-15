f4b55dc7be84b29b297dc491e51da5f898a6141e
Moritz <schradermoritz@web.de>
2026-05-15 06:35:47 -0400

    booksystem practice task added

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -26,7 +26,8 @@ function showbooks() {
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
         <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
-        <button onclick="editbook(${index})">Edit</button>`
+        <button onclick="editbook(${index})">Edit</button>
+        <button onclick="deletebook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -40,6 +41,11 @@ function editbook(index) {
     books.splice(index, 1); // Remove old entry
     showbooks(); // Refresh list
   }
+
+function deletebook(index) {
+    books = books.splice(index, 1);
+    showbooks();
+}
 
   function clearInputs() {
     document.getElementById('bookName').value = '';
